031338a6578fc2aa39969c7a64f01b3eee1dba1c
lroux07 <lroux@edenschool.fr>
2023-11-20 11:31:48 +0100

    Configurate projet

---
+++ main.js
@@ -0,0 +1,15 @@
+let a = document.querySelector("#a");
+let up_a = document.querySelector("#up_a");
+let down_a = document.querySelector("#down_a");
+
+let b = document.querySelector("#b");
+let up_b = document.querySelector("#up_b");
+let down_b = document.querySelector("#down_b");
+
+let c = document.querySelector("#c");
+let up_c = document.querySelector("#up_c");
+let down_c = document.querySelector("#down_c");
+
+let d = document.querySelector("#d");
+let up_d = document.querySelector("#up_d");
+let down_d = document.querySelector("#down_d");
